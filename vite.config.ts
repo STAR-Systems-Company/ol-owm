cc4991128f75e514aa7a3c5102e7bc897a8957ea
ihorkaleniuk <ihor.kalinuk@paytion.com>
2025-06-04 20:14:01 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  root: "demo",
+  root: "demo", // 👈 чтобы собралось из demo/
+  build: {
+    outDir: "../docs", // 👈 чтобы выгрузка шла в /docs для GitHub Pages
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@dist": path.resolve(__dirname, "dist/esm"),
+    },
+  },
 });
